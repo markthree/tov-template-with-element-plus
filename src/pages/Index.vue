8660a9acb10d9b0cec680d060b020441380c9153
markthree <1801982702@qq.com>
2022-02-25 20:14:41 +0800

    Initial commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const { t, locale } = useI18n()
+const { isDark, toggleDark } = useDarks()
+
+const toggleLocale = () => {
+    locale.value = locale.value === 'zh-CN' ? 'en' : 'zh-CN'
+}
+</script>
+
+<template>
+    <div class="m-6">Hello，This is the tov template！！</div>
+    <div class="cursor-pointer m-6" @click="toggleDark()">light: {{ isDark }} click me!!</div>
+
+    <div class="cursor-pointer m-6" @click="toggleLocale()">language: {{ t('index') }} click me!!</div>
+</template>
